7715ab6e73dd72cfad2508cb5cc541519d0901b0
Aurelio Joseph <aurelioaborges@gmail.com>
2022-12-28 13:58:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# guess-the-number
+
+A simple little game made as a small JavaScript learning project.
+
+# languages
+- JavaScript
+- HTML
+- CSS
+
+### play and enjoy ;)
+
+############################################
+
+# acerte-o-numero
+
+Um joguinho simples feito como mini projeto, em JavaScript.
+
+# languages
+- JavaScript
+- HTML
+- CSS
+
+### jogue e divirta-se ;)
+
+############################################
---
+++ script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// for Random Secret Number
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+// to show Secret Number
+// document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // for NO INPUT
+  if (!guess) {
+    displayMessage('⛔️ Número Inválido!');
+    document.querySelector('.message').style.fontSize = '2.5rem';
+
+    // when player WINS
+  } else if (guess === secretNumber) {
+    displayMessage('🥳 Você Acertou!!');
+
+    // to show Secret Number
+    document.querySelector('.number').textContent = secretNumber;
+
+    // to change Styles
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.message').style.fontSize = '3rem';
+    document.querySelector('.number').style.width = '35rem';
+
+    // highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // when guess is WRONG
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guess > secretNumber ? '📈 Muito Alto!' : '📉 Muito Baixo!'
+      );
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('😭 Você Perdeu!!');
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.message').style.fontSize = '3rem';
+    }
+  }
+});
+
+// restart button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  displayMessage('Comece a jogar...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').style.fontSize = '2rem';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#333';
+});
